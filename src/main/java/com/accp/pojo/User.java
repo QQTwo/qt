b28aa453add818324ccac2144385cf68d1ba9dd4
sunl <781265993@qq.com>
2019-02-18 16:45:16 +0800

    测试郭生强啊啊啊a

--- src/main/java/com/accp/pojo/User.java
+++ src/main/java/com/accp/pojo/User.java
@@ -3,4 +3,6 @@ package com.accp.pojo;
 public class User {
 	public String  ss = "我爱你";
 	public String  ssgg = "我爱ssds你";
+	public String  ssgg321 = "我爱ssds你";
+	public String  ssgg3213 = "我爱ssds你";
 }
